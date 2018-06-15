d4bf442315818c25cff7a2be62bef1440618d891
Stephen Gulland <Stephen Gulland@Obsidian-Brick>
2018-06-14 17:49:15 -0700

    Project with no files

---
+++ src/basic/Main.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yo what up chicken. - Allegra Frank.");
+	}
+
+}
